d8aace4ec923fe9d3b20a2a7c5a9a63ef76c5002
walkeknow <kewalshah077@gmail.com>
2020-05-06 01:12:37 +0530

    View API data on DOM

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -4,24 +4,43 @@ function handleSubmit(event) {
     // check what text was put into the form field
     let formText = document.getElementById('name').value
     let url = Client.checkURL(formText)
-    console.log(url)
-
     console.log("::: Form Submitted :::")
 
-    fetch('http://localhost:8000/test')
-        .then(res => res.json())
-        .then(function (res) {
-            console.log(res);
-            updateUI(res);
-        })
+    const inputObject = {
+        url,
+    }
 
-    /* Function to GET API Data from local server */
+    /* POST request to send user input to local server */
+    const sendData = async (url = '', data = {}) => {
+        console.log(`::: POST request sent :::`)
+
+        const response = await fetch(url, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+
+        try {
+            const responseData = response.json();
+            return responseData;
+        } catch (error) {
+            console.log("error", error)
+        }
+    }
+
     function updateUI(res) {
-        console.log(res);
         const entry = res.analyzedData;
         document.getElementById('tone').innerHTML = `<strong>Tone:</strong> ${entry.tone}`;
         document.getElementById('subjectivity').innerHTML = `<strong>Subjectivity:</strong> ${entry.subjectivity}`;
     }
+
+    sendData('/sendData', inputObject)
+    .then(function(res){
+        updateUI(res);
+    })
 }
 
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -6,9 +6,20 @@ const doetenv = require('dotenv');
 
 doetenv.config();
 
-const app = express()
+const app = express();
 
-app.use(express.static('dist'))
+/* Dependencies */
+const bodyParser = require('body-parser');
+
+//Configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.static('dist'));
 
 console.log(__dirname)
 
@@ -30,23 +41,26 @@ app.listen(8000, function () {
     console.log('Example app listening on port 8000!')
 })
 
-app.get('/test', function (req, res) {
-    // textapi.sentiment({
-    //     text: 'John is a very good football player',
-    //     mode: 'tweet',
-    // }, function (error, response) {
-    //     if (error === null) {
-    //         const analyzedData = {
-    //             tone: response.polarity,
-    //             subjectivity: response.subjectivity,
-    //         }
-    //         projectData["analyzedData"] = analyzedData;
-    //         console.log(projectData);
-    //         res.send(projectData);
-    //     } else {
-    //         console.log(error);
-    //         alert("Sorry! We cannot process your request at the moment")
-    //     }
-    // });
-    res.send(mockAPIResponse);
+let url = null
+
+app.post('/sendData', function (req, res) {
+    url = req.body.url;
+    projectData["url"] = url;
+    textapi.sentiment({
+        url: url,
+        mode: 'document',
+    }, function (error, response) {
+        if (error === null) {
+            const analyzedData = {
+                tone: response.polarity,
+                subjectivity: response.subjectivity,
+            }
+            projectData["analyzedData"] = analyzedData;
+            console.log(projectData);
+            res.send(projectData);
+        } else {
+            console.log(error);
+            alert("Sorry! We cannot process your request at the moment")
+        }
+    });
 })
