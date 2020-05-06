72d78f9d7c836b33c51c98fac0f9ddbd650bf02b
walkeknow <kewalshah077@gmail.com>
2020-05-06 22:26:40 +0530

    implement first Jest test

---
+++ src/__tests__/formHandler.test.js
@@ -0,0 +1,3 @@
+jest.mock('../__mocks__/formHandler');
+
+
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,38 @@
+/* POST request to send user input to local server */
+const sendData = async (url = '', data = {}) => {
+    console.log(`::: POST request sent :::`)
+
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    try {
+        const responseData = response.json();
+        return responseData;
+    } catch (error) {
+        document.getElementById('tone').innerHTML = ``;
+        console.log("error", error)
+    }
+}
+
+function submitForm(inputObject = {}) {
+    sendData('/sendData', inputObject)
+        .then(function (res) {
+            updateUI(res);
+        })
+}
+
+function updateUI(res) {
+    const entry = res.analyzedData;
+    document.getElementById('tone').innerHTML = `<span class="label">Tone:</span> ${entry.tone}`;
+    document.getElementById('subjectivity').innerHTML = `<span class="label">Subjectivity:</span> ${entry.subjectivity}`;
+}
+
 function handleSubmit(event) {
     event.preventDefault()
 
@@ -13,38 +48,8 @@ function handleSubmit(event) {
         url,
     }
 
-    /* POST request to send user input to local server */
-    const sendData = async (url = '', data = {}) => {
-        console.log(`::: POST request sent :::`)
-
-        const response = await fetch(url, {
-            method: 'POST',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        });
-
-        try {
-            const responseData = response.json();
-            return responseData;
-        } catch (error) {
-            document.getElementById('tone').innerHTML = ``;
-            console.log("error", error)
-        }
-    }
-
-    function updateUI(res) {
-        const entry = res.analyzedData;
-        document.getElementById('tone').innerHTML = `<span class="label">Tone:</span> ${entry.tone}`;
-        document.getElementById('subjectivity').innerHTML = `<span class="label">Subjectivity:</span> ${entry.subjectivity}`;
-    }
-
-    sendData('/sendData', inputObject)
-    .then(function(res){
-        updateUI(res);
-    })
+    submitForm(inputObject);
 }
 
 export { handleSubmit }
+export { sendData }
